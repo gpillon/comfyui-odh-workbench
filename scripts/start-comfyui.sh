cb922be7ac76183fc1365f7216845d378ab9a74d
gpillon <giorgio.pillon@live.com>
2025-06-22 22:42:15 +0200

    fixing buildconfig

--- scripts/start-comfyui.sh
+++ scripts/start-comfyui.sh
@@ -5,21 +5,11 @@ set -e
 # Change to the ComfyUI directory
 cd /opt/app-root/ComfyUI
 
-
-# Install ComfyUI Manager if not already installed
-if [ ! -d "custom_nodes/ComfyUI-Manager" ]; then
+# Install ComfyUI Manager if not already installed and DISABLE_MANAGER env variable is not equal to "true"
+if [ ! -d "custom_nodes/ComfyUI-Manager" ] && [ "${DISABLE_MANAGER}" != "true" ]; then
     echo "Installing ComfyUI Manager..."
-    mkdir -p custom_nodes
     git clone https://github.com/ltdrdata/ComfyUI-Manager.git custom_nodes/ComfyUI-Manager
 fi
-
-# # Fix permissions for OpenShift
-# if [ "$(id -u)" -ne 0 ]; then
-#     echo "Setting correct permissions for non-root user..."
-#     find /opt/app-root/ComfyUI -type d -exec chmod 775 {} \;
-#     find /opt/app-root/ComfyUI -type f -exec chmod 664 {} \;
-#     chmod +x /opt/app-root/ComfyUI/main.py
-# fi
 
 # Start ComfyUI
 echo "Starting ComfyUI on port ${PORT:-8080}..."
