926078f4e26277aef1d1ff741b5671e81130dcd7
gpillon <giorgio.pillon@live.com>
2025-06-23 04:23:35 +0200

    healthcheck fights

--- scripts/start-comfyui-with-nginx.sh
+++ scripts/start-comfyui-with-nginx.sh
@@ -11,8 +11,5 @@ fi
 /opt/app-root/scripts/run-nginx.sh &
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf &
 
-# Start ComfyUI with runtime flags
-/opt/app-root/scripts/start-comfyui.sh $RUNTIME_FLAGS $EXTRA_FLAGS &
-
 # Keep the container running
 tail -f /dev/null 
